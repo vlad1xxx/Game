b5d53a864e5e5b893d0c081ce81308f060237bd4
Ha4trueKa4 <kelemandean76@gmail.com>
2024-02-20 16:16:42 +0300

    загрузка уровня tiled

---
+++ maps/t3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="t3" tilewidth="160" tileheight="160" tilecount="96" columns="12">
+ <image source="../tiles/t3.png" width="1920" height="1280"/>
+</tileset>
